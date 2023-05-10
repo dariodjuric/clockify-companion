4589c7010f8ae7f02e19958d17468316b5b7c16a
Dario Djuric <dario@thisdot.co>
2023-05-10 22:28:58 +0200

    Use server action processing

--- src/components/report-input-form/report-input-form.tsx
+++ src/components/report-input-form/report-input-form.tsx
@@ -1,8 +1,12 @@
-import type { PropFunction, Signal } from "@builder.io/qwik";
-import { $, component$ } from "@builder.io/qwik";
+import type { Signal } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import { z } from "@builder.io/qwik-city";
 import type { InitialValues } from "@modular-forms/qwik";
-import { useForm, zodForm$ } from "@modular-forms/qwik";
+import { formAction$, useForm, zodForm$ } from "@modular-forms/qwik";
+import type { Invoice } from "~/lib/invoice-generator";
+import { generateInvoice } from "~/lib/invoice-generator";
+import { endOfMonth, setSeconds, startOfMonth } from "date-fns";
+import InvoiceDisplay from "~/components/invoice-display/invoice-display";
 
 const loginSchema = z.object({
   workspaceId: z.string(),
@@ -13,51 +17,70 @@ export type LoginForm = z.infer<typeof loginSchema>;
 
 interface Props {
   initialValues: Signal<InitialValues<LoginForm>>;
-  onSubmit: PropFunction<(values: LoginForm) => void>;
 }
 
-export default component$<Props>(({ initialValues: loader, onSubmit }) => {
-  const [, { Form, Field }] = useForm<LoginForm>({
-    loader,
-    validate: zodForm$(loginSchema),
-  });
+export const useFormAction = formAction$<LoginForm, Invoice>(async (values) => {
+  const startOfMonthDate = startOfMonth(new Date(values.month));
+  const endOfMonthDate = setSeconds(endOfMonth(new Date(values.month)), 59);
 
-  const handleSubmit = $((values: LoginForm) => {
-    onSubmit(values);
+  const invoice = await generateInvoice(
+    values.workspaceId,
+    values.apiKey,
+    startOfMonthDate,
+    endOfMonthDate
+  );
+
+  return {
+    status: "success",
+    message: " ",
+    data: invoice,
+  };
+}, zodForm$(loginSchema));
+
+export default component$<Props>(({ initialValues }) => {
+  const [loginForm, { Form, Field }] = useForm<LoginForm, Invoice>({
+    loader: initialValues,
+    validate: zodForm$(loginSchema),
+    action: useFormAction(),
   });
 
   return (
-    <Form onSubmit$={handleSubmit}>
-      <Field name="workspaceId">
-        {(field, props) => (
-          <>
-            <input
-              {...props}
-              type="text"
-              autoComplete="on"
-              value={field.value}
-            />
-          </>
-        )}
-      </Field>
-      <Field name="apiKey">
-        {(field, props) => (
-          <>
-            <input
-              {...props}
-              type="password"
-              autoComplete="on"
-              value={field.value}
-            />
-          </>
-        )}
-      </Field>
-      <Field name="month">
-        {(field, props) => (
-          <input {...props} type="month" value={field.value} />
-        )}
-      </Field>
-      <input type="submit" />
-    </Form>
+    <>
+      <Form>
+        <Field name="workspaceId">
+          {(field, props) => (
+            <>
+              <input
+                {...props}
+                type="text"
+                autoComplete="on"
+                value={field.value}
+              />
+            </>
+          )}
+        </Field>
+        <Field name="apiKey">
+          {(field, props) => (
+            <>
+              <input
+                {...props}
+                type="password"
+                autoComplete="on"
+                value={field.value}
+              />
+            </>
+          )}
+        </Field>
+        <Field name="month">
+          {(field, props) => (
+            <input {...props} type="month" value={field.value} />
+          )}
+        </Field>
+        <input type="submit" />
+      </Form>
+      {loginForm.response.data && (
+        <InvoiceDisplay invoice={loginForm.response.data} />
+      )}
+    </>
   );
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,9 @@
-import { $, component$, useSignal } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import type { InitialValues } from "@modular-forms/qwik";
 import type { LoginForm } from "~/components/report-input-form/report-input-form";
 import ReportInputForm from "~/components/report-input-form/report-input-form";
-import type { Invoice } from "~/lib/invoice-generator";
-import { generateInvoice } from "~/lib/invoice-generator";
-import InvoiceDisplay from "~/components/invoice-display/invoice-display";
-import {
-  endOfMonth,
-  format,
-  setSeconds,
-  startOfMonth,
-  subMonths,
-} from "date-fns";
+import { format, subMonths } from "date-fns";
 
 export const useFormLoader = routeLoader$<InitialValues<LoginForm>>(() => {
   const currentDate = new Date();
@@ -28,24 +19,9 @@ export const useFormLoader = routeLoader$<InitialValues<LoginForm>>(() => {
 
 export default component$(() => {
   const initialValues = useFormLoader();
-  const invoiceSignal = useSignal<Invoice | null>(null);
-
-  const submitHandler = $(async (values: LoginForm) => {
-    const startOfMonthDate = startOfMonth(new Date(values.month));
-    const endOfMonthDate = setSeconds(endOfMonth(new Date(values.month)), 59);
-
-    invoiceSignal.value = await generateInvoice(
-      "",
-      "",
-      startOfMonthDate,
-      endOfMonthDate
-    );
-  });
-
   return (
     <>
-      <ReportInputForm initialValues={initialValues} onSubmit={submitHandler} />
-      {invoiceSignal.value && <InvoiceDisplay invoice={invoiceSignal.value} />}
+      <ReportInputForm initialValues={initialValues} />
     </>
   );
 });
